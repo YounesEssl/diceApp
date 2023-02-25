e388929188b6bb84be7ed37a6ed7269cc2f1fbd6
benvire <91453870+benvire@users.noreply.github.com>
2023-02-25 16:08:38 +0000

    adding gameList

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -22,6 +22,7 @@ export default function GameList() {
   //   });
   // };
 
+  // fonction qui récupère tous ce qui ce trouve dans la table gamList
   const fetchPost = async () => {
     await getDocs(collection(db, "gameList")).then((querySnapshot) => {
       const newData = querySnapshot.docs.map((doc) => ({
@@ -40,6 +41,8 @@ export default function GameList() {
   return (
     <>
       {/* <button onClick={addGame}>add game list</button> */}
+
+      {/* button qui permet d'afficher ou nan la liste de jeux en changeant le la variable showList par true ou false */}
       <button
         onClick={() => {
           showList ? setShowList(false) : setShowList(true);
@@ -47,6 +50,8 @@ export default function GameList() {
       >
         Show List
       </button>
+
+      {/* balise html pour faire un tableau */}
       <table hidden={showList}>
         <thead>
           <tr>
@@ -56,6 +61,15 @@ export default function GameList() {
           </tr>
         </thead>
         <tbody>
+          {/* code permettant de faire la liste des jeux qui ce trouve dans la table gamList 
+          
+          - Le .map() récupère chaque objets de jeux
+          - Les mets dans le tableau (la balise et non un array)
+          - On récupère ce que l'on veut avec le game.name|quantity|condition
+
+          - Le key={i} est la pour faire jolie et pour pas qu'on est une erreur dans la console
+          
+          */}
           {gameList?.map((game, i) => {
             return (
               <tr key={i}>
