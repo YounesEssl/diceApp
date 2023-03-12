fb229a4f70f1ef177a4e869ed5daad9fd1b21382
TheD0Om <98453055+TheD0Om@users.noreply.github.com>
2023-03-12 14:53:21 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'; // import du fichier CSS
+import { db } from './firebase.js';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      games: [],
+      selectedGame: null
+    };
+  }
+
+  componentDidMount() {
+    // Récupération des jeux depuis Firestore
+    db.collection('games').get()
+      .then(querySnapshot => {
+        const games = querySnapshot.docs.map(doc => doc.data());
+        this.setState({ games });
+      })
+      .catch(error => console.log(error));
+  }
+
+  selectRandomGame = () => {
+    const { games } = this.state;
+    const randomIndex = Math.floor(Math.random() * games.length);
+    const selectedGame = games[randomIndex];
+    this.setState({ selectedGame });
+  }
+
+  render() {
+    const { selectedGame } = this.state;
+    return (
+      <div>
+        <button onClick={this.selectRandomGame}>Choisir un jeu aléatoire</button>
+        {selectedGame && (
+          <div>
+            <h2>{selectedGame.name}</h2>
+            <p>{selectedGame.description}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
